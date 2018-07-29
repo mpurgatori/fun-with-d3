becaca08901cefb7b09973fc11f668c96d51b504
Mike Purgatori <mpurgatori@gmail.com>
2018-07-29 16:02:06 -0500

    adition of jasmine console reporter

--- spec/support/spec-runner-config.js
+++ spec/support/spec-runner-config.js
@@ -1,8 +1,22 @@
 const path = require('path');
 
+const JasmineConsoleReporter = require('jasmine-console-reporter');
 var Jasmine = require('jasmine');
 var HtmlReporter = require('jasmine-pretty-html-reporter').Reporter;
 var jasmine = new Jasmine();
+
+const reporter = new JasmineConsoleReporter({
+  colors: 1,           // (0|false)|(1|true)|2
+  cleanStack: 1,       // (0|false)|(1|true)|2|3
+  verbosity: 4,        // (0|false)|1|2|(3|true)|4
+  listStyle: 'indent', // "flat"|"indent"
+  activity: false,     // boolean or string ("dots"|"star"|"flip"|"bouncingBar"|...)
+  emoji: true,
+  beep: true
+});
+
+jasmine.env.clearReporters();
+jasmine.addReporter(reporter);
 
 jasmine.loadConfigFile('spec/support/jasmine.json');
 
@@ -12,4 +26,5 @@ jasmine.addReporter(new HtmlReporter({
 }));
 
 jasmine.execute();
+
 
